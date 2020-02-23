fd8d1b2d2b24968188f0582922e2c939815a68b2
Jaime Loeuf <jaimeloeuf@gmail.com>
2020-02-23 17:46:14 +0800

    Created mock_data and logic for prefilledForms: 1.  Removed data.js mock_data 2.  Created mock_data file for the prefilledForms 3.  Added a getter function for prefilledForm in vuex store.

--- src/store/index.js
+++ src/store/index.js
@@ -2,51 +2,36 @@ import Vue from "vue";
 import Vuex from "vuex";
 
 // Mock server data
-import data from "./mock_data/data";
+import data from "./mock_data/prefilledForms";
 
 // Import server communication modules
 import users from "./users";
 
 // Import serverless function call
-import notifyManager from "./notifyManager";
+// import notifyManager from "./notifyManager";
+
+import prefilledForms from "./mock_data/prefilledForms";
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
-    email: "",
-    name: "",
-    ipads: {},
-    walkies: {},
-    problems: [],
+    user: {
+      email: "",
+      name: ""
+    },
     history: []
   },
   mutations: {
     // Update email function, updates the user's name too
     update_email: function(state, new_email) {
-      state.email = new_email;
+      state.user.email = new_email;
     },
     update_name: function(state, new_name) {
       // Defaults to name associated with the email from server data
       new_name = new_name || users.getUsername(state.email);
 
-      state.name = new_name;
-    },
-    new_problem: async function(state, new_problem_object) {
-      // Get the time where problem is reported at
-      const date = new Date();
-      const timestamp = date.getTime();
-
-      // Use timestamp as a unique problem ID for v-bind:key in Faulty-list
-      new_problem_object.id = timestamp;
-      new_problem_object.timestamp = timestamp;
-      new_problem_object.time = date.toString();
-
-      state.problems.push(new_problem_object);
-
-      // Remove problem id before emailing DTM as its not needed
-      delete new_problem_object.id;
-      await notifyManager("", new_problem_object);
+      state.user.name = new_name;
     }
   },
   actions: {
@@ -54,5 +39,9 @@ export default new Vuex.Store({
       commit("update_data", data);
     }
   },
-  modules: {}
+  getters: {
+    prefilledForm: state => {
+      return prefilledForms[state.user.email];
+    }
+  }
 });
