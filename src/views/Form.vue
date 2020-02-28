6e12ec1e515fcefc829aef5a3950ba6755c88129
Jaime Loeuf <jaimeloeuf@gmail.com>
2020-02-29 01:19:12 +0800

    Fixed Form view and prefill generation: 1.  Fixed the form view to make it work. 2.  Updated the form view to now generate prefill content and load the     prefilled google form.

--- src/views/Form.vue
+++ src/views/Form.vue
@@ -6,7 +6,6 @@
     <!-- Iframe defaults to the non prefilled form -->
     <iframe
       id="google_form"
-      src="https://docs.google.com/forms/d/e/1FAIpQLSc2vDScbMRJ9YWeDM9Q99jl_gJm4YVpYOGX3i4hCGmASHyVyw/viewform?embedded=true"
       height="1120"
       frameborder="0"
       scrolling="no"
@@ -23,28 +22,44 @@ export default {
     NavigationBar
   },
   data() {
-    const userPrefilledForm = this.$store.state.forms[
-      this.$store.state.user.email
-    ];
-    if (userPrefilledForm) this.loadPreFilledForm(userPrefilledForm);
-
     return {
       notice: "Loading standup form…"
     };
+  },
+  mounted() {
+    this.loadForm();
   },
   methods: {
     formLoaded() {
       console.log("google form loaded");
       this.notice = "";
     },
-    loadPreFilledForm(formURL) {
+    loadForm(formURL) {
       // Scroll to the top of the page
       window.scrollTo(0, 0);
 
-      // Update user that pre filled form is found
-      this.notice = "Found pre filled form, loading it now...";
+      // Default embeddable form
+      formURL =
+        "https://docs.google.com/forms/d/e/1FAIpQLSc2vDScbMRJ9YWeDM9Q99jl_gJm4YVpYOGX3i4hCGmASHyVyw/viewform?embedded=true";
 
-      // Update the source of the iframe to load the new pre-filled form
+      /* Format the date nicely so it can used in the URL string for getting a pre-filled form */
+      const date = new Date();
+      const currentMonth =
+        date.getMonth() + 1 < 10
+          ? `0${date.getMonth() + 1}`
+          : date.getMonth() + 1;
+      const currentDate =
+        date.getDate() < 10 ? `0${date.getDate()}` : date.getDate();
+      const datePreFill = `&entry.1613076786=${date.getFullYear()}-${currentMonth}-${currentDate}`;
+      console.log(`Date used for form prefill: ${datePreFill}`);
+      formURL += datePreFill;
+
+      const userName = `&entry.2047896550=${this.$store.state.user.name}`;
+      console.log(`Name used for form prefill: ${userName}`);
+      formURL += userName;
+
+      // Update source of iframe to load pre-filled form
+      console.log("Form URL used: ", formURL);
       document.getElementById("google_form").src = formURL;
     }
   }
